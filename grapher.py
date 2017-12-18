8477c505c2c61f55372a0a2aa67e1e017a0caac1
Kaveh Pezeshki <kaveh@pezeshki.org>
2017-12-18 19:13:34 +0000

    UI changes

--- grapher.py
+++ grapher.py
@@ -26,30 +26,30 @@ def setup():
     #setting up window
     root = Tk()
     root.title("MagnetTest V0.1")
-    root.resizable(width=False, height=False)
+    #root.resizable(width=False, height=False)
     root.geometry('{}x{}'.format(320, 240))
     app = Window(root)
 
     #setting up textboxes
 
     #user instructions
-    instr = Text(root, height=1, width=50)
-    instr.tag_configure("center", justify="center")
+    instr = Text(root, height=5, width=70)
+    instr.tag_configure("center", justify="center", font=("Helvetica", 20))
     instr.insert("1.0","RED=CALIBRATE | BLACK=TEST")
     instr.tag_add("center", "1.0", "end")
     instr.pack()
     #testing status
-    teststat = Text(root, height = 1, width=50, font=("Helvetica", 32))
+    teststat = Text(root, height = 1, width=50, font=("Helvetica", 42))
     teststat.tag_configure("center", justify="center")
     teststat.insert("2.0","NO DATA")
     teststat.tag_add("center", "1.0", "end")
     teststat.pack()
     #sensor values
-    xval_label = Label(root, text="x=0.000 g", font=("Helvetica",16))
+    xval_label = Label(root, text="x=0.000 g", font=("Helvetica",36))
     xval_label.pack(padx=5,pady=10,side=LEFT)
-    yval_label = Label(root, text="y=0.000 g", font=("Helvetica",16))
+    yval_label = Label(root, text="y=0.000 g", font=("Helvetica",36))
     yval_label.pack(padx=5,pady=10,side=LEFT)
-    zval_label = Label(root, text="z=0.000 g", font=("Helvetica",16))
+    zval_label = Label(root, text="z=0.000 g", font=("Helvetica",36))
     zval_label.pack(padx=5,pady=10,side=RIGHT)
 
     root.update()
