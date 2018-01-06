2e394f78c80e42293f62f9dc1e2863b109dc5f9e
Kaveh Pezeshki <kaveh@pezeshki.org>
2018-01-05 16:31:49 -0800

    redid UI with grid framework, added offset display and color coding, bug fixes

--- grapher.py
+++ grapher.py
@@ -9,7 +9,8 @@ import sensor
 import time
 import RPi.GPIO as GPIO
 import math
-
+from PIL import ImageTk
+from PIL import Image
 
 #set the maximum number of digits in the output
 DIGITS=5
@@ -31,6 +32,9 @@ def setup_gpio():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    #setting up sensor
+    sensor.setup_magnetometer()
+
 
 def setup_ui():
     '''Adds UI elements to Window'''
@@ -42,9 +46,13 @@ def setup_ui():
     global yval_label
     global zval_label
     global tot_label
+    global xoff_label
+    global yoff_label
+    global zoff_label
 
-    #setting up sensor
-    sensor.setup_magnetometer()
+    #to prevent image from being garbage collected
+    global logo
+    global logo_panel
 
     #setting up window parameters
     root = Tk()
@@ -54,49 +62,77 @@ def setup_ui():
 
     #setting up textboxes
 
+    #inserting Kaiam logo
+    logo = ImageTk.PhotoImage(Image.open("/home/pi/kaiamproject/kaiam.png"))
+    logo_panel = Label(root, image=logo)
+    logo_panel.logo = logo
+    logo_panel.grid(row=0,column=0)
     #user instructions
-    instr = Text(root, height=5, width=70)
-    instr.tag_configure("center", justify="center", font=("Helvetica", 20))
-    instr.insert("1.0","RED=CALIBRATE | BLACK=TEST")
-    instr.tag_add("center", "1.0", "end")
-    instr.pack()
+    instr = Label(root, text="RED=CALIBRATE | BLACK=TEST", font=("Helvetica", 20))
+    instr.grid(row=0, column=1, columnspan=2)
     #testing status
-    teststat = Text(root, height = 1, width=50, font=("Helvetica", 42))
-    teststat.tag_configure("center", justify="center")
-    teststat.insert("2.0","NO DATA")
-    teststat.tag_add("center", "1.0", "end")
-    teststat.pack()
+    teststat = Label(root, text="NO DATA", font=("Helvetica", 42))
+    teststat.configure(foreground="orange")
+    teststat.grid(row=1, column=0, columnspan=3)
     #sensor magnitude
-    tot_label = Text(root, height = 1, width=50, font=("Helvetica", 30))
-    tot_label.tag_configure("center", justify="center")
-    tot_label.insert("2.0","Total Field Strength = 0.000 mgauss")
-    tot_label.tag_add("center", "1.0", "end")
-    tot_label.pack()
+    spacer_1  = Label(root, text=" ").grid(row=2,column=1)
+    tot_label = Label(root, text="Total Field Strength = 0.000 mgauss", font=("Helvetica", 30))
+    tot_label.grid(row=3, column=0, columnspan=3)
     #sensor values
-    xval_label = Label(root, text="x=0.000 mg", font=("Helvetica",36))
-    xval_label.pack(padx=5,pady=10,side=LEFT)
-    yval_label = Label(root, text="y=0.000 mg", font=("Helvetica",36))
-    yval_label.pack(padx=5,pady=10,side=LEFT)
-    zval_label = Label(root, text="z=0.000 mg", font=("Helvetica",36))
-    zval_label.pack(padx=5,pady=10,side=RIGHT)
-
+    sensor_label = Label(root, font=("Helvetica", 26), text=u"Current \u0394 From Offset")
+    sensor_label.grid(row=4, column=0, columnspan=3)
+    xval_label = Label(root, text="x=0.000 mg", font=("Helvetica",26))
+    xval_label.grid(row=5,column=0)
+    yval_label = Label(root, text="y=0.000 mg", font=("Helvetica",26))
+    yval_label.grid(row=5,column=1)
+    zval_label = Label(root, text="z=0.000 mg", font=("Helvetica",26))
+    zval_label.grid(row=5,column=2)
+    #offset values
+    offset_label = Label(root, font=("Helvetica", 26), text="Offset Values")
+    offset_label.grid(row=6, column=0, columnspan=3)
+    xoff_label = Label(root, text="x=0.000 mg", font=("Helvetica",26))
+    xoff_label.grid(row=7,column=0)
+    yoff_label = Label(root, text="y=0.000 mg", font=("Helvetica",26))
+    yoff_label.grid(row=7,column=1)
+    zoff_label = Label(root, text="z=0.000 mg", font=("Helvetica",26))
+    zoff_label.grid(row=7,column=2)
     #updating Window
     root.update()
 
 def sensortest():
     '''reads sensor data and updates Window fields, as well as calculating whether the tested isolator is in spec'''
 
-    #reading the maggnetometer
+    #reading the magnetometer
     (xval, yval, zval) = sensor.read_magnetometer()
     sensor.calc_status((xval, yval, zval))
     sensor_status = sensor.get_status()
 
-    #clearing the sensor status field
-    teststat.delete("1.0","11.0")
-    teststat.insert("1.0", sensor_status)
+    #setting the sensor status field
+    teststat.configure(text=sensor_status)
+
+    #updating the sensor status field color
+    if sensor_status == "INIT FAILED" or sensor_status == "TEST FAILED":
+        teststat.configure(foreground="red")
+    elif sensor_status == "NO DATA":
+        teststat.configure(foreground="orange")
+    elif sensor_status == "TEST PASSED":
+        teststat.configure(foreground="green")
 
     #updating magnetic field strength measurements and Window
     update_magnetometer()
+
+def update_offsets():
+    '''reads offsets and updates Window fields'''
+    #reading offset data
+    (xoff, yoff, zoff) = sensor.get_offsets()
+
+    #updating data fields
+    xoff_label.config(text="x="+str(xoff)[0:DIGITS]+" mg")
+    yoff_label.config(text="y="+str(yoff)[0:DIGITS]+" mg")
+    zoff_label.config(text="z="+str(zoff)[0:DIGITS]+" mg")
+
+    #updating Window
+    root.update()
 
 def update_magnetometer():
     '''reads sensor data and updates Window fields'''
@@ -106,19 +142,14 @@ def update_magnetometer():
     #calculating magnetic field strength
     mag = math.sqrt(xval**2 + yval**2 + zval**2)
 
-    #to ensure that the y-value of magnetic field strength remains centered
-    spacing = 5-len(str(xval))
-
     #updating data fields
-    xval_label.config(text="x="+str(xval)[0:DIGITS]+" mg"+spacing*" ")
+    xval_label.config(text="x="+str(xval)[0:DIGITS]+" mg")
     yval_label.config(text="y="+str(yval)[0:DIGITS]+" mg")
     zval_label.config(text="z="+str(zval)[0:DIGITS]+" mg")
-    tot_label.delete("1.0","11.0")
-    tot_label.insert("1.0","Total Field Strength = "+str(mag)[0:DIGITS]+" mgauss")
+    tot_label.configure(text="Total Field Strength = "+str(mag)[0:DIGITS]+" mgauss")
 
     #updating Window
     root.update()
-
 
 def main():
     '''provides top-level control. Magnetic field strength readings are constantly updating, and calibration and sensor tests are controlled by the red and black buttons'''
@@ -131,8 +162,13 @@ def main():
     #main loop
     while True:
         #button control
-        if (not GPIO.input(23)): sensor.calibrate()
-        if (not GPIO.input(24)): sensortest()
-
-        else:                    update_magnetometer()
+        if (not GPIO.input(23)):
+            sensor.calibrate()
+            update_offsets()
+        if (GPIO.input(24)):
+            sensortest()
+        else:
+            update_magnetometer()
         time.sleep(0.1)
+
+main()
--- sensor.py
+++ sensor.py
@@ -45,11 +45,14 @@ def calc_status(mag):
     #The current metric tests whether the X, Y, and Z-axis magnetic field strength values are within the bounds of X_RANGE, Y_RANGE, and Z_RANGE.
     #   These bounds are set at +/- 1 Gauss- this was an arbitrary value, and will need to be adjusted depending on the isolator.
     if mag_x <= X_RANGE[0] or mag_x >= X_RANGE[1] or mag_y <= Y_RANGE[0] or mag_y >= Y_RANGE[1] or mag_z <= Z_RANGE[0] or mag_z >= Z_RANGE[1]:
-        status = "FAILED"
-    else: status = "PASSED"
+        status = "TEST FAILED"
+    else: status = "TEST PASSED"
 
 def setup_magnetometer():
     '''Initializes communication with the magnetometer and sets sensor parameters'''
+
+    #setting global variables
+    global status
 
     #setting global parameters
     global lsm303
@@ -66,27 +69,40 @@ def setup_magnetometer():
 def read_magnetometer():
     '''reads the magnetometer, returning a tuple of x,y, and z values adjusted by scale and offsets'''
 
+    #setting global variables
+    global status
+
     #(0,0,0) is returned if there is a sensor or communication failure
     if status == "INIT FAILED": return (0,0,0)
-    accel, mag = lsm303.read()
-
-    #calculation of adjusted values
-    return (mag[0]*scale[0]-offsets[0], mag[1]*scale[1]-offsets[1], mag[2]*scale[2]-offsets[2])
+    try:
+        accel, mag = lsm303.read()
+        #calculation of adjusted values
+        return (mag[0]*scale[0]-offsets[0], mag[1]*scale[1]-offsets[1], mag[2]*scale[2]-offsets[2])
+    except:
+        status = "INIT FAILED"
+        return (0,0,0)
 
 def calibrate():
     '''calibrates the magnetometer to the current set of values by assigning magnetometer values to offsets'''
 
     #declared as global as offsets are modified
     global offsets
+    global status
 
     #no calibration is done if there is a sensor failure
     if status == "INIT FAILED": return
-    accel, mag = lsm303.read()
-
-    #assigning offsets to be read magnetometer values
-    offsets = mag
+    try:
+        accel, mag = lsm303.read()
+        #assigning offsets to be read magnetometer values
+        offsets = (mag[0]*scale[0], mag[1]*scale[1], mag[2]*scale[2])
+    except:
+        status = "INIT FAILED"
 
 def get_status():
     '''returns the result of the most recent magnetometer test'''
     return status
+
+def get_offsets():
+    '''returns the current offsets'''
+    return offsets
 
